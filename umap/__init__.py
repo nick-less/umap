c721b9b58d8060baef4a249f117539b0f95da27c
nick-less <nick-less@gmx.de>
2021-01-11 19:00:34 +0100

    removed old code

--- umap/__init__.py
+++ umap/__init__.py
@@ -1,11 +1,8 @@
 "Create maps with OpenStreetMap layers in a minute and embed them in your site."
 
-#VERSION = (1, 2, 3)
-
 with open("umap/version.txt") as v_file:
   __version__ = v_file.read()
 
 __author__ = 'Yohan Boniface'
 __contact__ = "ybon@openstreetmap.fr"
 __homepage__ = "https://github.com/umap-project/umap"
-#__version__ = ".".join(map(str, VERSION))
